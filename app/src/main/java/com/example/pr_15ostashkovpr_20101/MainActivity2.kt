3c5a5bf72135fc2444ee4b6144f4395d1d0ab8bb
User <ostashkovladislav@yandex.ru>
2022-06-17 19:53:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr_15ostashkovpr_20101/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.pr_15ostashkovpr_20101
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+    fun onClickStepsActivity3(view: View){
+        val intent = Intent(this, MainActivity3::class.java)
+        startActivity(intent)
+    }
+}
